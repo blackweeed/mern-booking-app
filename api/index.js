82140200d7044377f0aee7c4bac2d858b8580c3e
blackweeed <blackweeeed@gmail.com>
2023-03-31 17:48:51 +0200

    adding user registration

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const User = require("./models/User");
 require("dotenv").config();
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(
@@ -19,15 +22,18 @@ app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  User.create({
-    name,
-    email,
-    password,
-  });
-
-  res.json({ name, email, password });
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (err) {
+    res.status(422).json(e);
+  }
 });
 
 app.listen(4000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,13 +7,14 @@ const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const registerUser = (e) => {
+  const registerUser = async (e) => {
     e.preventDefault();
-    axios.post("/register", {
+    await axios.post("/register", {
       name,
       email,
       password,
     });
+    alert("Regeistration succesfull");
   };
 
   return (
